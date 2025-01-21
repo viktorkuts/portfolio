0ac57102ed7e99edb5996d17a42d8e31d01fcf90
Viktor Kuts <vik.kuts@hotmail.com>
2025-01-21 13:42:20 -0500

    init

---
+++ be/src/main/java/com/viktorkuts/portfolio_be/PortfolioBeApplication.java
@@ -0,0 +1,13 @@
+package com.viktorkuts.portfolio_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBeApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/com/viktorkuts/portfolio_be/PortfolioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viktorkuts.portfolio_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
